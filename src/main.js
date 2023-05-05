390f764edfb3b1c79fd9d05237efd3747b14dbbc
MuhammadFadhilah0063 <uwaiesm@gmail.com>
2023-05-05 15:15:28 +0800

    installation and configure bootstrap, popper js, axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 
-import './assets/main.css'
+// Bootstrap
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+
+// Configure Axios
+axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'http://localhost:8000/'
 
 const app = createApp(App)
 
